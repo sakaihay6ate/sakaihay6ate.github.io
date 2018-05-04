02653ae3dd905010052aedcd0c35c86d85f8c357
RueichiLin-PC\RueichiLin <rueichilin@astrocorp.com.tw>
2018-05-04 17:34:09 +0800

    nodeJs practice

--- nodejsPractice/requestHandlers.js
+++ nodejsPractice/requestHandlers.js
@@ -1,8 +1,58 @@
-start=()=>{
-    console.log(`Request handler 'start' was called.`);
+// const exec=require("child_process").exec;
+const querystring=require("querystring"),
+        fs=require("fs"),
+        formidable=require("formidable");
+function start(response) {
+    console.log("Request handler 'start' was called.");
+    let body = '<html>' +
+        '<head>' +
+        '<meta http-equiv="Content-Type" content="text/html; ' +
+        'charset=UTF-8" />' +
+        '</head>' +
+        '<body>' +
+        '<form action="/upload" enctype="multipart/form-data" method="post">' +
+        // '<textarea name="text" rows="20" cols="60"></textarea>' +
+        '<input type="file" name="upload">' +
+        '<input type="submit" value="Submit Upload file" />' +
+        '</form>' +
+        '</body>' +
+        '</html>';
+
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.write(body);
+    response.end();
 }
-upload=()=>{
+
+upload=(response,request)=>{
     console.log(`Request handler 'upload' was called.`);
+
+    let form = new formidable.IncomingForm();
+    console.log(`about to parse`);
+    form.parse(request,(error,fields,files)=>{
+        console.log(`parsing done`);
+        fs.renameSync(files.upload.path,"./tmp/test.png");
+        response.writeHead(200, { "content-Type": "text/plain" });
+        response.write("received image:<br/>");
+        response.write(`<img src='/show'/>`);
+        response.end();
+    })
+
+}
+
+function show(response) {
+    console.log("Request handler 'show' was called.");
+    fs.readFile("./tmp/test.png", "binary", function (error, file) {
+        if (error) {
+            response.writeHead(500, { "Content-Type": "text/plain" });
+            response.write(error + "\n");
+            response.end();
+        } else {
+            response.writeHead(200, { "Content-Type": "image/png" });
+            response.write(file, "binary");
+            response.end();
+        }
+    });
 }
 exports.start=start;
 exports.upload=upload;
+exports.show=show;
--- nodejsPractice/router.js
+++ nodejsPractice/router.js
@@ -1,9 +1,12 @@
-route=(handle,pathname)=>{
+route=(handle,pathname,response,request)=>{
     console.log(`About to route a request for ${pathname}`);
-    if(typeof handle[pathname]==="function"){
-        handle[pathname]();
+    if (typeof handle[pathname] === 'function'){
+        return handle[pathname](response,request);
     }else{
         console.error(`No request handler found for ${pathname}.`);
+        response.writeHead(404,{"content-Type":"text/plain"});
+        response.write("404 Not found");
+        response.end();
     }
 }
 exports.route=route;
