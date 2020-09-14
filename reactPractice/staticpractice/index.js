e72a7c42d79eb3815cd9497304b6a865d695c2bf
RueichiLin-PC\RueichiLin <rueichilin@astrocorp.com.tw>
2020-09-14 16:20:07 +0800

    test

--- reactPractice/staticpractice/index.js
+++ reactPractice/staticpractice/index.js
@@ -1,6 +1,6 @@
 // import React from 'react';
 // import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
 function Square(props) {
   return(
     <button className="square" onClick={props.onClick}>
